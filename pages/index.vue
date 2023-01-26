35be07e43f767133b8c5907e70a6de78b767eff6
HalmarBergman <halmar.bergman@student.uva.nl>
2023-01-26 05:54:53 +0100

    check

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,36 @@
           <h1 class="my-name" :class="{ textdark: store.lightOrDark == true }">Bergman</h1>
         </div>
         <Navbar />
+        <div class="content">
+          <div class="first-section">
+            <div class="image">
+              <img src="~/assets/images/test.jpg" />
+            </div>
+            <div class="text">
+              <h3 :class="{ textdark: store.lightOrDark == true }">Me, myself and I</h3>
+              <h6 :class="{ textdark: store.lightOrDark == true }">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </h6>
+            </div>
+          </div>
+          <div class="second-section">
+            <div class="text">
+              <h6 :class="{ textdark: store.lightOrDark == true }">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </h6>
+            </div>
+            <div class="image">
+              <img src="~/assets/images/test.jpg" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -30,4 +60,64 @@ function mode() {
 
 <style scoped lang="scss">
 @import '@/assets/css/variables.scss';
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px 0px 0px;
+  width: 100%;
+  height: 100%;
+  gap: 40px;
+  overflow-y: scroll;
+  .first-section {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 25px;
+    width: 100%;
+    .image {
+      min-width: 150px;
+      min-height: 150px;
+      max-width: 250px;
+      max-height: 150px;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    .text {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 0px 14px 0px 0px;
+    }
+  }
+  .second-section {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 25px;
+    width: 100%;
+    .image {
+      min-width: 150px;
+      min-height: 150px;
+      max-width: 400px;
+      max-height: 150px;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    .text {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 0px 14px 0px 0px;
+    }
+  }
+}
 </style>
