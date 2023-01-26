60ec74fdc6ac72cf7046726871a8d8ebb9538e2c
HalmarBergman <halmar.bergman@student.uva.nl>
2023-01-26 05:00:39 +0100

    pages

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="contact">contact</div>
+  <div class="contact">
+    <div class="background" :class="{ backgrounddark: store.lightOrDark === 'dark' }">
+      <div class="section" :class="{ sectiondark: store.lightOrDark === 'dark' }">
+        <div class="title">
+          <h1 class="my-name" :class="{ textdark: store.lightOrDark === 'dark' }">Halmar</h1>
+          <h1 class="my-name" :class="{ textdark: store.lightOrDark === 'dark' }">Henrik Bjarne</h1>
+          <h1 class="my-name" :class="{ textdark: store.lightOrDark === 'dark' }">Bergman</h1>
+        </div>
+        <Navbar />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const store = useNuxtStore()
+</script>
 
 <style scoped lang="scss">
 @import '@/assets/css/variables.scss';
